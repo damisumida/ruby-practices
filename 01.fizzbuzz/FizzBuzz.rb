7680c3fdb39a119564572c92414aca490b4755a6
damisumida <gjptmda628@gmail.com>
2022-03-11 07:20:30 +0900

    change FizzBuzz.rb

--- 01.fizzbuzz/FizzBuzz.rb
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -1,11 +1,9 @@
 1.upto(20) { |i|
-  if i % 3 == 0
-    if i % 5 == 0
-      puts 'FizzBuzz'
-    else
-      puts 'Fizz'
-    end
-  elseif i % 5 == 0
+  if i % 3 == 0 && i % 5 == 0
+    puts 'FizzBuzz'
+  elsif i % 3 == 0
+    puts 'Fizz'
+  elsif i % 5 == 0
     puts 'Buzz'
   else
     puts i
