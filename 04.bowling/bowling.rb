625d969fb6979467cd38808b1b80a1884a303d75
damisumida <gjptmda628@gmail.com>
2022-03-21 20:18:25 +0900

    スペル、strike?メソッドを修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -23,34 +23,32 @@ def add_point(throw, inputs)
 end
 
 def strike?(throw, inputs)
-  return true if inputs[throw] == 'X'
-
-  false
+  inputs[throw] == 'X'
 end
 
 score = 0
-flame_point = 0
-is_flame_end = false
+frame_point = 0
+is_frame_end = false
 inputs = ARGV[0].split(',')
 throw_time = inputs.length
 
 throw_time.times do |throw|
   if strike?(throw, inputs)
     score += add_strike_point(throw, inputs)
-    flame_point = 0
-    is_flame_end = false
+    frame_point = 0
+    is_frame_end = false
     break if throw_time == throw + 3 # 10フェーズでストライクになったら終了
 
     next
   end
 
   score += add_point(throw, inputs)
-  flame_point += add_point(throw, inputs)
+  frame_point += add_point(throw, inputs)
 
-  score += add_spare_point(throw, inputs) if flame_point == 10
+  score += add_spare_point(throw, inputs) if frame_point == 10
 
-  flame_point = 0 if is_flame_end
-  is_flame_end = !is_flame_end
+  frame_point = 0 if is_frame_end
+  is_frame_end = !is_frame_end
 end
 
 puts score
