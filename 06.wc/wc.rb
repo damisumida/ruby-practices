d97208e2e77aabcb2731b4f2be2eae081c4abc80
damisumida <gjptmda628@gmail.com>
2022-09-28 10:35:22 +0900

    結果出力位置を修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -8,7 +8,7 @@ def main
   filename_list = build_filename_list
   file_info_list = build_file_info_list(filename_list)
   file_info_list.push(calc_total(file_info_list)) if file_info_list.size != 1
-  show_file_info_list(file_info_list)
+  show_file_info_list(file_info_list, filename_list)
 end
 
 def build_filename_list
@@ -35,9 +35,9 @@ end
 def build_file_info(file_status, file_name)
   option = search_option
   file_info = {}
-  file_info['line_num'] = file_status.count("\n") unless option[:w] || option[:c]
-  file_info['word_num'] = file_status.split(/\s+/).size unless option[:l] || option[:c]
-  file_info['file_size'] = file_status.size unless option[:l] || option[:w]
+  file_info['line_num'] = file_status.count("\n") if option[:l] || option_all_unchecked?(option)
+  file_info['word_num'] = file_status.split(/\s+/).size if option[:w] || option_all_unchecked?(option)
+  file_info['file_size'] = file_status.size if option[:c] || option_all_unchecked?(option)
   file_info['file_name'] = file_name
   file_info
 end
@@ -53,12 +53,20 @@ def search_option
   option
 end
 
+def option_all_unchecked?(option)
+  if option[:l] || option[:w] || option[:c]
+    false
+  else
+    true
+  end
+end
+
 def calc_total(file_info_list)
   option = search_option
   file_info = {}
-  file_info['line_num'] =  sum_file_info(file_info_list, 'line_num') unless option[:w] || option[:c]
-  file_info['word_num'] =  sum_file_info(file_info_list, 'word_num') unless option[:l] || option[:c]
-  file_info['file_size'] =  sum_file_info(file_info_list, 'file_size') unless option[:l] || option[:w]
+  file_info['line_num'] =  sum_file_info(file_info_list, 'line_num') if option[:l] || option_all_unchecked?(option)
+  file_info['word_num'] =  sum_file_info(file_info_list, 'word_num') if option[:w] || option_all_unchecked?(option)
+  file_info['file_size'] =  sum_file_info(file_info_list, 'file_size') if option[:c] || option_all_unchecked?(option)
   file_info['file_name'] =  'total'
   file_info
 end
@@ -67,8 +75,8 @@ def sum_file_info(file_info_list, key_name)
   file_info_list.sum { |info| info[key_name] }
 end
 
-def show_file_info_list(file_info_list)
-  word_size = build_word_size(file_info_list)
+def show_file_info_list(file_info_list, filename_list)
+  word_size = build_word_size(file_info_list, filename_list)
   file_info_list.each do |info|
     info.each do |key_value|
       key = key_value[0]
@@ -79,29 +87,26 @@ def show_file_info_list(file_info_list)
   end
 end
 
-def build_word_size(file_info_list)
-  option = search_option
+def build_word_size(file_info_list, filename_list)
   word_size = []
-  word_size.push(default_word_size(file_info_list)) unless option[:l] || option[:w] || option[:c]
+  word_size.push(default_word_size(filename_list))
   word_size.push(file_info_list.map { |info| info['line_num'].to_s.size }.max)
   word_size.push(file_info_list.map { |info| info['word_num'].to_s.size }.max)
   word_size.push(file_info_list.map { |info| info['file_size'].to_s.size }.max)
   word_size.max
 end
 
-def default_word_size(file_info_list)
-  if file_info_list.size >= 1
+def default_word_size(filename_list)
+  if filename_list.size >= 1
     4
-  elsif file_info_list.size.zero?
+  elsif filename_list.size.zero?
     7
-  else
-    0
   end
 end
 
 def show_item(key, value, word_size)
-  if key == :file_name
-    print value
+  if key == 'file_name'
+    print value.to_s.ljust(1)
   else
     print value.to_s.rjust(word_size)
   end
