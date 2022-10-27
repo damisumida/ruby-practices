a5440a6345273a02eb83404e0e783415c2fb9922
damisumida <gjptmda628@gmail.com>
2022-10-27 13:38:49 +0900

    - create_keynamesメソッドを削除 - オプション引数とkeynameが一致するよう修正 - 結果出力時の処理を修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,11 +4,11 @@
 require 'optparse'
 require 'readline'
 
-DEFAULT_LENGTH = 4
+TEXT_LENGTH = 4
 
 def main
   option = parse_argv
-  file_info_list = build_file_info_list(option[:filenames])
+  file_info_list = build_file_info_list(option['file_name'])
   file_info_list.push(calc_total(file_info_list)) if file_info_list.size != 1
   show_file_info_list(file_info_list, option)
 end
@@ -16,12 +16,12 @@ end
 def parse_argv
   option = Hash.new(false)
   OptionParser.new do |opt|
-    opt.on('-l') { |v| option[:l] = v }
-    opt.on('-w') { |v| option[:w] = v }
-    opt.on('-c') { |v| option[:c] = v }
+    opt.on('-l') { |v| option['line_num'] = v }
+    opt.on('-w') { |v| option['word_num'] = v }
+    opt.on('-c') { |v| option['file_size'] = v }
     opt.parse!(ARGV)
   end
-  option[:filenames] = ARGV
+  option['file_name'] = ARGV
   option
 end
 
@@ -58,34 +58,22 @@ def calc_total(file_info_list)
 end
 
 def show_file_info_list(file_info_list, option)
-  keynames = create_keynames(option)
+  all_unchecked = !(option['line_num'] || option['word_num'] || option['file_size'])
   word_size = calc_wordsize(file_info_list)
   file_info_list.each do |info|
     info.each do |key, value|
-      show_item(key, value, word_size) if keynames.include?(key)
+      show_item(key, value, word_size) if all_unchecked || option[key]
     end
     puts
   end
 end
 
 def calc_wordsize(file_info_list)
-  word_size = [DEFAULT_LENGTH]
+  word_size = [TEXT_LENGTH]
   %w[line_num word_num file_size].each do |key|
     word_size.push(file_info_list.map { |info| info[key].to_s.size })
   end
   word_size.flatten.max
-end
-
-def create_keynames(option)
-  all_unchecked = !(option[:l] || option[:w] || option[:c])
-  return %w[line_num word_num file_size file_name] if all_unchecked
-
-  key = []
-  key.push('line_num') if option[:l]
-  key.push('word_num') if option[:w]
-  key.push('file_size') if option[:c]
-  key.push('file_name')
-  key
 end
 
 def show_item(key, value, word_size)
@@ -93,8 +81,8 @@ def show_item(key, value, word_size)
     print value.to_s
   else
     print value.to_s.rjust(word_size)
+    print ' '
   end
-  print ' ' if key != 'file_name'
 end
 
 main
